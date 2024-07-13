145b1afb29e7afdc642e3d2177b3f46fdf613702
kunal101994 <satyanarayanrout101994@gmail.com>
2024-07-13 22:32:03 +0530

    30 Days_coding_Challenges

---
+++ Variable&Datatypes.js
@@ -0,0 +1,57 @@
+// Activity:1
+// Task:1
+var number = 123;
+console.log(number);
+
+// Task: 1
+let name = "Satyanarayan";
+console.log(name);
+
+// Activity:2
+// Task:3
+let boolean = true;
+console.log(boolean);
+
+// Activity:3
+// Task:4
+
+//Number
+let score = 420;
+console.log(typeof score);
+
+// String
+let bookName = "Javascript";
+console.log(typeof bookName);
+
+// Boolean
+let isStudent = true;
+console.log(typeof isStudent);
+
+// object
+let myResume = {
+    name: "Satyanarayan",
+    age: 25,
+    edu: "MCA",
+}
+
+console.log(typeof myResume);
+
+// Array
+let marvelMovies = ["IronMan", "Captain America", "Spider Man"];
+console.log(Array.isArray(marvelMovies));
+
+// Activity: 4
+// Task:5
+let firstName = "Satya";
+console.log(firstName);
+firstName = "Rudra";
+console.log(firstName);
+
+//Activity 5
+// Task:6
+const myScore = 100;
+console.log(myScore);
+// reassign the value
+myScore = 124
+console.log(myScore);
+// TypeError: Assignment to constant variable.
